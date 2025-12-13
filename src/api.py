2fd92ec551224257d4cc0468370c2b043ccddd6d
ana-sher <nshcherbakovaa@gmail.com>
2025-12-13 15:52:59 +0100

    fix

--- src/api.py
+++ src/api.py
@@ -2,6 +2,9 @@ from datetime import datetime, timezone
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 from titiler.core.factory import TilerFactory
 from src.api import climate_model
 from api.core.logging import setup_logging
@@ -28,7 +31,7 @@ cog = TilerFactory()
 
 app.include_router(cog.router, tags=["Cloud Optimized GeoTIFF"])
 app.include_router(climate_model.router, tags=["Climate Model API"])
-
+limiter = Limiter(key_func=get_remote_address)
 app.state.limiter = limiter
 
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
@@ -41,7 +44,7 @@ async def root():
 
 @app.get("/health")
 async def health():
-    return Response(status_code=status.HTTP_200_OK)
+    return Response(status_code=200)
 
 # handler = Mangum(
 #     app
